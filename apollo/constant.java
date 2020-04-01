c0f155223894e4b21284efebfb799f4a075d5f0d
liorsagy <62811252+liorsagy@users.noreply.github.com>
2020-04-02 02:30:49 +0300

    Update constant.java

--- apollo/constant.java
+++ apollo/constant.java
@@ -6,15 +6,15 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 //TODO fill all the variables
 public class constant {
 
-    public static double DEGREE_PER_TICK = 0;
+    public static final double DEGREE_PER_TICK = 0;
 
-    public static double Kp_moduleRotation;
+    public static final double Kp_moduleRotation = 0;
 
-    public static int offSet = 0;
+    public static final int offSet = 0;
 
-    public static double wheelDiameter = 0;
+    public static final double wheelDiameter = 0;
 
-    static PIDFCoefficients arm = new PIDFCoefficients(0,0,0,0, MotorControlAlgorithm.PIDF);
+    static final PIDFCoefficients arm = new PIDFCoefficients(0,0,0,0, MotorControlAlgorithm.PIDF);
 
 
 }
